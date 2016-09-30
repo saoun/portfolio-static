3cbf4ab27b7eaa44275b7b8787029565bc0b28dd
sarah aoun <sarah@silk.co>
2016-09-30 14:43:46 -0400

    edit server.rb

--- webportfolio/server.rb
+++ webportfolio/server.rb
@@ -3,5 +3,27 @@ module Sinatra
     get "/" do
       erb :index
     end
+
+    get '/about' do
+      erb :index
+    end
+
+    get '/workshops' do
+      erb :index
+    end
+
+    get '/cv' do
+      erb :index
+    end
+
+    get '/projects' do
+      erb :index
+    end
+
+    get '/contact' do
+      erb :index
+    end
+
+
   end
 end
