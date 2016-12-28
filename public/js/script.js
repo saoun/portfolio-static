c4e8cf6d94ff618ecaad358691730c2fc2b16b29
sarah aoun <sarah@silk.co>
2016-12-27 21:56:43 -0500

    fix img

--- public/js/script.js
+++ public/js/script.js
@@ -1,4 +1,24 @@
 "use strict";
 (function(){
 
+  console.log('helloooo');
+
+  $('.projectBack').hover(function(){
+    $(this).find('img').fadeTo(200, 0.5);
+    $(this).find('p').fadeTo(200, 1);
+  }, function(){
+    $(this).find('img').fadeTo(200, 1);
+    $(this).find('p').fadeTo(200, 0)
+  })
+
+
+
+
+
 })();
+
+
+
+
+
+
